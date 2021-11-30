ecab19a79f6c318900ca1d00f8c77743d2bd5c3a
oleksii.brihida <oleksii.brihida@idealscorp.com>
2021-11-30 18:29:57 +0200

    Next commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,16 +1,20 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
-
+import com.codeborne.selenide.selector.ByText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class FirstTest {
     @Test
     public void userCanSearchKeywordWithGoogle(){
-        open("http://google.com");
-        $(By.name("q")).setValue("Selenide").pressEnter();
-        $(By.name("q")).setValue("No-No-No").pressEnter();
-        $(By.name("q")).setValue("Go-go-go").pressEnter();
+        open("http://automationpractice.com/index.php");
+        $("a.login").click();
+        $("input#email_create").setValue("tastus@gmail.com");
+        $(ByText("Create an account")).click();
+        sleep (5000);
+
+             //   .setValue("Selenide").pressEnter();
+
     }
 
 }
