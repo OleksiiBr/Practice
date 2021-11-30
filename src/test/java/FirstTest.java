b2b84086a0d0f0ef02d277980bda6e2586a6a7f9
oleksii.brihida <oleksii.brihida@idealscorp.com>
2021-11-30 19:18:21 +0200

    Next commit

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,20 +1,27 @@
 import org.junit.Test;
-import org.openqa.selenium.By;
-import com.codeborne.selenide.selector.ByText;
+
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
+
 
 public class FirstTest {
     @Test
-    public void userCanSearchKeywordWithGoogle(){
+    public void UserRegister(){
         open("http://automationpractice.com/index.php");
         $("a.login").click();
-        $("input#email_create").setValue("tastus@gmail.com");
-        $(ByText("Create an account")).click();
-        sleep (5000);
-
-             //   .setValue("Selenide").pressEnter();
-
+        $("input#email_create").setValue("tastus@gmail.com").pressEnter();
+        sleep (2000);
+        $("input#id_gender1").click();
+        $("input#customer_firstname").setValue("Tastus");
+        $("input#customer_lastname").setValue("Tester");
+        $("input#passwd").setValue("123123");
+        $("input#firstname").setValue("Tastus");
+        $("input#lastname").setValue("Tester");
+        $("input#address1").setValue("City bla street bla-bla and appartment la-la-la");
+        $("input#city").setValue("Ci-city");
+        $("input#postcode").setValue("12345");
+        $("select#id_state").selectOption(3);
+        $("input#phone_mobile").setValue("12345");
+        $("button#submitAccount").click();
+        sleep (20000);
     }
-
 }
